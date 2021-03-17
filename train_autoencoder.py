107a03a68b9fc12fa76ebd2fe63320be60217e40
pauloesampaio <paulo.ed@gmail.com>
2021-03-16 23:08:24 -0300

    Try removing flips

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -18,48 +18,48 @@ X_train, X_val, y_train, y_val = train_test_split(
     X, y, test_size=0.2, random_state=12345
 )
 
-print("Blocking image by binary code size")
-modified_tiles = []
-for im in X_train.copy():
-    tiles = generate_tiles(im, config["binary_code_size"], config["binary_code_size"])
-    modified_tiles = modified_tiles + tiles
-
-original_tiles = []
-for im in y_train.copy():
-    tiles = generate_tiles(im, config["binary_code_size"], config["binary_code_size"])
-    original_tiles = original_tiles + tiles
-
-X_train_small = (
-    np.array(modified_tiles)
-    .reshape(-1, config["binary_code_size"] * config["binary_code_size"])
-    .mean(axis=1)
-    .reshape(
-        X_train.shape[0],
-        X_train.shape[1] // config["binary_code_size"],
-        X_train.shape[1] // config["binary_code_size"],
-    )
-)
-
-y_train_small = (
-    np.array(original_tiles)
-    .reshape(-1, config["binary_code_size"] * config["binary_code_size"])
-    .mean(axis=1)
-    .reshape(
-        y_train.shape[0],
-        y_train.shape[1] // config["binary_code_size"],
-        y_train.shape[1] // config["binary_code_size"],
-    )
-)
+# print("Blocking image by binary code size")
+# modified_tiles = []
+# for im in X_train.copy():
+#    tiles = generate_tiles(im, config["binary_code_size"], config["binary_code_size"])
+#    modified_tiles = modified_tiles + tiles
+#
+# original_tiles = []
+# for im in y_train.copy():
+#    tiles = generate_tiles(im, config["binary_code_size"], config["binary_code_size"])
+#    original_tiles = original_tiles + tiles
+#
+# X_train_small = (
+#    np.array(modified_tiles)
+#     .reshape(-1, config["binary_code_size"] * config["binary_code_size"])
+#     .mean(axis=1)
+#     .reshape(
+#         X_train.shape[0],
+#         X_train.shape[1] // config["binary_code_size"],
+#         X_train.shape[1] // config["binary_code_size"],
+#     )
+# )
+#
+# y_train_small = (
+#     np.array(original_tiles)
+#     .reshape(-1, config["binary_code_size"] * config["binary_code_size"])
+#     .mean(axis=1)
+#     .reshape(
+#         y_train.shape[0],
+#         y_train.shape[1] // config["binary_code_size"],
+#         y_train.shape[1] // config["binary_code_size"],
+#     )
+# )
 
 print("Tiling images")
 X_tiles = []
-for each_image in X_train_small:
+for each_image in X_train:
     current_tiles = generate_tiles(each_image, config["tile_side"], config["step_size"])
     X_tiles = X_tiles + current_tiles
 X_tiles = np.array(X_tiles)
 
 y_tiles = []
-for each_image in y_train_small:
+for each_image in y_train:
     current_tiles = generate_tiles(each_image, config["tile_side"], config["step_size"])
     y_tiles = y_tiles + current_tiles
 y_tiles = np.array(y_tiles)
@@ -68,7 +68,7 @@ X_tiles = X_tiles.reshape(X_tiles.shape + (1,))
 y_tiles = y_tiles.reshape(y_tiles.shape + (1,))
 
 augmentator = ImageDataGenerator(
-    horizontal_flip=True, vertical_flip=True, validation_split=0.2
+    horizontal_flip=False, vertical_flip=False, validation_split=0.2
 )
 
 train_generator = augmentator.flow(X_tiles, y_tiles, seed=12345, subset="training")
